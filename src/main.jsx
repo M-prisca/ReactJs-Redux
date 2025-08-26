62c24938a83985f5e3928ab9bb6fdb8e1f8c24e5
Prisca Masereli <priscamasereli@gmail.com>
2025-08-26 10:51:13 +0200

    Feat: Creating a redux Store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
 
+const store = configureStore({
+  reducer: {},
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Login />
-    <Profile />
+    <Provider store={store}>
+      <App />
+      <Login />
+      <Profile />
+    </Provider>
   </StrictMode>
 );
